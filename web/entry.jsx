b1e9a8555ed83fff05d1271d5ec5a9b928616690
wangwei703 <wangwei703@163.com>
2017-09-23 16:25:33 +0800

    09-23 submit -3

--- web/entry.jsx
+++ web/entry.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import initReactFastclick from 'react-fastclick';
 
-// initReactFastclick();
+initReactFastclick();
 window.DPR=window.devicePixelRatio||1;
 ReactDOM.render(
   <App />,
